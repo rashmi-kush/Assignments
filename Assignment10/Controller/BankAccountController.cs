c4aef8fd3ce63f4a443de4252ac96b9cf4c95d9b
CMHLDTIT07 <CMHLDTIT07@ITESDT07>
2025-05-21 17:36:36 +0530

    Add project files.

---
+++ Assignment10/Controller/BankAccountController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Principal;
+
+namespace Assignment10.Controller
+{
+    public class BankAccountController : ControllerBase
+    {
+        [Route("/")]
+        public string Start()
+        {
+            Response.StatusCode = 200;
+            return "Welcome to the Best Bank.";
+        }
+        [Route("/account-details")]
+        public IActionResult AccountDetails()
+        {
+            var Accnt_Details = new
+            {
+                accountNumber = 1001,
+                accountHolderName = "Example Name",
+                currentBalance = 5000
+            };
+            Response.StatusCode = 200;
+            return new JsonResult(Accnt_Details);
+        }
+        [Route("/account-statement")]
+        public IActionResult AccountStatement()
+        {
+            Response.StatusCode = 200;
+            return File("statement.pdf", "application/pdf");
+        }
+
+        [Route("/get-current-balance/{accountNumber:int?}")]
+        public IActionResult GetData()
+        {
+            object accountNumObj;
+            if (HttpContext.Request.RouteValues.TryGetValue("accountNumber", out accountNumObj) && accountNumObj is string accountNumber)
+            {
+
+                if (string.IsNullOrEmpty(accountNumber))
+                {
+                    return NotFound("Account Number should be supplied.");
+                }
+
+                if (int.TryParse(accountNumber, out int accountnum))
+                {
+                    var Accnt_Details = new
+                    {
+                        accountNumber = 1001,
+                        accountHolderName = "Example Name",
+                        currentBalance = 5000
+                    };
+                    if (accountnum == 1001)
+                    {
+                        return Content(Accnt_Details.currentBalance.ToString());
+                    }
+                    else
+                    {
+                        return BadRequest("Account Number should be 1001.");
+                    }
+                }
+                else
+                {
+                    return BadRequest("Invalid Account number");
+                }
+
+            }
+            else
+            {
+                return NotFound("Account not supplied.");
+            }
+        }
+    }
+}
